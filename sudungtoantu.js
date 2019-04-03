a792a1b3d91c47a4c18cc45c431065c00cd96a0f
lexuanthiencodegym <http.acoustic.com@gmail.com>
2019-04-03 18:42:02 +0700

    file sudungtoantu.js

---
+++ sudungtoantu.js
@@ -0,0 +1,13 @@
+var width;
+var height;
+
+var width = parseInt(prompt("Enter the width"));
+document.write("Enter the width: " + width);
+document.write("<br/>");
+
+var height = parseInt(prompt("Enter the height"));
+document.write("Enter the height: " + height);
+document.write("<br/>");
+
+var area = width*height;
+document.write("The area is: " + area);
